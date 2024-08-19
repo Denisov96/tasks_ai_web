13e769e537393ba9d02ad224b88b8dcc8174f76c
Denisov Anton <denisovant406@gmail.com>
2024-08-19 18:27:25 +0300

    gitpong 3

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,11 @@
-
-import {Heading} from "../components/Heading"
-
+import { Heading } from "../components/Heading";
 
 export default function Page() {
   return (
     <>
-      <Heading text ="headings" />
-      <p>Hello gitpong</p>
+      <Heading text="headings" />
       <p>Hello gitpong 2</p>
+      <p>Hello gitpong 3</p>
     </>
   );
 }
