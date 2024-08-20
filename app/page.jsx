42ea3b66610b296c44cc179f829f5b035f97d2cd
Denisov Anton <denisovant406@gmail.com>
2024-08-20 19:53:43 +0300

    changed the name of the element (.checkbox) seems to have formatted the code and changed the area of ​​the checkbox and the colors in it

--- app/page.jsx
+++ app/page.jsx
@@ -3,11 +3,10 @@ import { Task } from "../components/Task";
 export default function Page() {
   return (
     <>
-      <Task text="Buy Milk"/>
-      <Task text="Repair the door"/>
-      <Task text="Throw garbage"/>
-      <Task text="Cook a dinner"/>
+      <Task text="Buy Milk" />
+      <Task text="Repair the door" />
+      <Task text="Throw garbage" />
+      <Task text="Cook a dinner" />
     </>
-    
   );
 }
--- components/Task/index.jsx
+++ components/Task/index.jsx
@@ -1,21 +1,20 @@
 "use client";
 import { useState } from "react";
-import styles from "./styles.module.css"; 
+import styles from "./styles.module.css";
 
 export function Task({ text }) {
   const [completed, setCompleted] = useState(false);
 
   function handleClick() {
-    setCompleted(!completed); 
+    setCompleted(!completed);
   }
 
   return (
     <div onClick={handleClick} className={styles.taskCard}>
-      <div className={`${styles.button} ${completed ? styles.completed : ''}`}>
-        {completed && <span className={styles.checkmark}>✔</span>}
+      <div className={`${styles.button} ${completed ? styles.completed : ""}`}>
+        {completed && <span className={styles.checkbox}>✔</span>}
       </div>
       <span className={styles.text}>{text}</span>
     </div>
   );
 }
-
