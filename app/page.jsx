c0d0629363f2e1c1e25d11c952e2132d44f8f3b7
Denisov Anton <denisovant406@gmail.com>
2024-08-24 20:26:29 +0300

    drag and drop tasks

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,33 @@
+"use client";
+import { useState } from "react";
 import { Task } from "../components/Task";
 
 export default function Page() {
+  const [tasks, setTasks] = useState([
+    { id: 1, text: "Buy Milk" },
+    { id: 2, text: "Repair the door" },
+    { id: 3, text: "Throw garbage" },
+    { id: 4, text: "Cook a dinner" },
+  ]);
+
+  const moveTask = (fromIndex, toIndex) => {
+    const updatedTasks = [...tasks];
+    const [movedTask] = updatedTasks.splice(fromIndex, 1);
+    updatedTasks.splice(toIndex, 0, movedTask);
+    setTasks(updatedTasks);
+  };
+
   return (
     <>
-      <Task text="Buy Milk" />
-      <Task text="Repair the door" />
-      <Task text="Throw garbage" />
-      <Task text="Cook a dinner" />
+      {tasks.map((task, index) => (
+        <Task
+          key={task.id}
+          id={task.id}
+          text={task.text}
+          index={index}
+          moveTask={moveTask}
+        />
+      ))}
     </>
   );
 }
--- components/Task/index.jsx
+++ components/Task/index.jsx
@@ -1,16 +1,41 @@
 "use client";
 import { useState } from "react";
+import { useDrag, useDrop } from "react-dnd";
 import styles from "./styles.module.css";
 
-export function Task({ text }) {
+const ItemType = "TASK";
+
+export function Task({ id, text, index, moveTask }) {
   const [completed, setCompleted] = useState(false);
 
-  function handleClick() {
-    setCompleted((completed) => !completed)
-  }
+  const [, ref] = useDrop({
+    accept: ItemType,
+    hover(item) {
+      if (item.index !== index) {
+        moveTask(item.index, index);
+        item.index = index;
+      }
+    },
+  });
+
+  const [{ isDragging }, drag] = useDrag({
+    type: ItemType,
+    item: { id, index },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  const handleClick = () => {
+    setCompleted((completed) => !completed);
+  };
 
   return (
-    <div onClick={handleClick} className={styles.taskCard}>
+    <div
+      ref={(node) => drag(ref(node))}
+      onClick={handleClick}
+      className={`${styles.taskCard} ${isDragging ? styles.dragging : ""}`}
+    >
       <div className={`${styles.button} ${completed ? styles.completed : ""}`}>
         {completed && <span className={styles.checkbox}>✔</span>}
       </div>
