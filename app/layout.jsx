87e8a57df557f2e3be25474dffcef799bfcda20e
Denisov Anton <denisovant406@gmail.com>
2024-10-01 22:42:31 +0300

    add new font

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,20 +2,22 @@
 import React from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import { Sometype_Mono } from "next/font/google";
+import { Roboto } from "next/font/google"; 
 
-const sometypeMono = Sometype_Mono({
-  weight: "400",
-  subsets: ["latin"],
+const roboto = Roboto({
+  weight: ["400", "700"], 
+  subsets: ["latin", "cyrillic"], 
   display: "swap",
 });
 
 export default function RootLayout({ children }) {
   return (
     <DndProvider backend={HTML5Backend}>
-      <html lang="en" className={sometypeMono.className}>
+      <html lang="en" className={roboto.className}> 
         <body>{children}</body>
       </html>
     </DndProvider>
   );
 }
+
+
