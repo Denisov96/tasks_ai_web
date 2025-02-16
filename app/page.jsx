f2aa83ea825bd2c822e9f26df13f5de71903d197
web-dev-lessons <info.offcast@gmail.com>
2025-02-16 13:20:40 +0100

    adds sign in view on FE

--- app/page.jsx
+++ app/page.jsx
@@ -1,53 +1,12 @@
 "use client";
-import { useState, useEffect } from "react";
-import { Logo } from "../components/Logo";
-import { TaskInput } from "../components/TaskInput";
-import { TaskList } from "../components/TaskList";
-import styles from "../styles.module.css";
-import { getTasks, updateTask, createTask } from "../lib/requests";
+import { useState } from "react";
+import { LoginView } from "../views/LoginView";
+import { TasksView } from "../views/TasksView";
 
 export default function Page() {
-  const [tasks, setTasks] = useState([]);
-  const [taskToEdit, setTaskToEdit] = useState(null);
+  const [currentUser, setCurrentUser] = useState();
 
-  useEffect(() => {
-    async function fetchAndSetTasks() {
-      const tasks = await getTasks();
-      setTasks(tasks);
-    }
-    fetchAndSetTasks();
-  }, []);
+  if (!currentUser) return <LoginView onSuccess={(user) => setCurrentUser(user)}  />;
 
-  async function handleSubmit() {
-    if (taskToEdit.text.trim() === "") return;
-
-    const newTasks = taskToEdit.id
-    ? await updateTask(taskToEdit)
-    : await createTask(taskToEdit.text);
-
-    setTasks(newTasks);
-    setTaskToEdit(null);
-  }
-
-  return (
-    <div className={styles.pageContainer}>
-      <Logo />
-
-      <TaskInput
-        value={taskToEdit?.text}
-        onSubmit={handleSubmit}
-        onChange={(value) => setTaskToEdit({ ...taskToEdit, text: value })}
-      />
-
-      <hr />
-
-      <TaskList
-        tasks={tasks}
-        onChange={(newTasks) => setTasks(newTasks)}
-        onEdit={(task) => {
-          setTaskToEdit(task);
-        }}
-      />
-    </div>
-  );
+  return <TasksView />;
 }
---
+++ views/LoginView.jsx
@@ -0,0 +1,30 @@
+import { useRef } from "react";
+
+export function LoginView(props) {
+  const userNameInputRef = useRef();
+
+  async function handleSignIn() {
+    const userName = userNameInputRef.current.value;
+
+    const response = await fetch("http://localhost:3000/api/signIn", {
+      method: "POST",
+      body: userName,
+    });
+
+    if (!response.ok) {
+      console.error(`Cannot log in user. Response status ${response.status}`);
+      return;
+    }
+
+    const currentUser = await response.json();
+
+    props.onSuccess(currentUser);
+  }
+
+  return (
+    <div>
+      <input ref={userNameInputRef} placeholder="Username" type="text" />
+      <button onClick={handleSignIn}>Sign In</button>
+    </div>
+  );
+}
