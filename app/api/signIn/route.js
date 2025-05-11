7027325af93ab507e748a2cd659fad36a27dcb10
Seva Deriushkin <seva.deriushkin@gmail.com>
2025-05-11 18:26:21 +0200

    backend can handle user password

--- app/api/signIn/route.js
+++ app/api/signIn/route.js
@@ -1,20 +1,38 @@
 import { prisma } from "../../../prisma/db";
+import bcrypt from "bcrypt";
+
+async function hashPassword(password) {
+  const salt = await bcrypt.genSalt(12)
+  const hash = await bcrypt.hash(password, salt)
+
+  return hash
+}
 
 export async function POST(request) {
-  const userName = await request.text();
+  const { username, password } = await request.json();
 
   let user = await prisma.user.findFirst({
     where: {
-      name: userName,
+      name: username,
     },
   });
 
   if (user === null) {
     user = await prisma.user.create({
       data: {
-        name: userName,
+        name: username,
+        password: await hashPassword(password),
       },
     });
+  }
+
+  const match = await bcrypt.compare(password, user.password)
+
+  if (match === false) {
+    return Response.json({
+      data: null,
+      errror: "Wrong password"
+    })
   }
 
   return Response.json({
