36bf7271044718194c3824c421c04bb6a1658bc0
Denisov Anton <denisovant406@gmail.com>
2025-06-30 20:43:24 +0300

    adds taskInput hook

--- components/TaskInput/index.jsx
+++ components/TaskInput/index.jsx
@@ -1,43 +1,22 @@
 import { TrashIcon, PlusIcon } from "../Icons/icons";
 import styles from "./styles.module.css";
-import { deleteTask, fetchTasks } from "../../lib/requests";
+import { useTaskInput } from "../../hooks/useTaskInput";
 
-export function TaskInput({
-  onSubmit,
-  value,
-  onChange,
-  tasks = [],
-  userId,
-  onChangeTasks,
-}) {
-  const hasCompletedTasks = tasks.some((task) => task.completed);
-
-  const deleteCompletedTasks = async () => {
-    const completedIds = tasks.filter((t) => t.completed).map((t) => t.id);
-
-    if (!completedIds.length) {
-      console.warn("No completed tasks to delete.");
-      return;
-    }
-
-    try {
-      for (const taskId of completedIds) {
-        await deleteTask(taskId, userId);
-      }
-
-      const updatedTasks = await fetchTasks(userId);
-      onChangeTasks && onChangeTasks(updatedTasks);
-    } catch (error) {
-      console.error("Delete error:", error.message);
-    }
-  };
+export function TaskInput({ tasks, userId, onChangeTasks }) {
+  const {
+    value,
+    hasCompletedTasks,
+    onChange,
+    onSubmit,
+    deleteCompletedTasks,
+  } = useTaskInput({ tasks, userId, onChangeTasks });
 
   return (
     <div className={styles.inputContainer}>
       <input
         type="text"
         autoFocus
-        value={value || ""}
+        value={value}
         onChange={(e) => onChange(e.target.value)}
         onKeyDown={(e) => e.key === "Enter" && onSubmit()}
         placeholder="Enter a new task..."
