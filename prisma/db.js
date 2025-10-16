d838502545e277c85647d67a9c59dd4dc077d211
Denisov Anton <denisovant406@gmail.com>
2025-10-16 23:32:45 +0300

    migration

--- prisma/db.js
+++ prisma/db.js
@@ -5,9 +5,7 @@ let prisma;
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
+  if (!global.prisma) global.prisma = new PrismaClient();
   prisma = global.prisma;
 }
 
