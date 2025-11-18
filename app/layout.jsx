fa1f89c9ff63b6436d1a9ed430dffab87baa579d
Denisov Anton <denisovant406@gmail.com>
2025-11-18 21:24:33 +0300

    transferring the menu to the component page + fix layout and css transferring

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,9 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { usePathname } from "next/navigation";
+import { useState } from "react";
 import { Roboto } from "next/font/google";
 import "../vars.css";
 import { UserContext } from "../context/userContext";
-import Sidebar from "../components/Sidebar";
-import { useAuth } from "../hooks/useAuth";
 
 const roboto = Roboto({
   subsets: ["latin", "cyrillic"],
@@ -15,27 +12,12 @@ const roboto = Roboto({
 });
 
 export default function RootLayout({ children }) {
-  const pathname = usePathname();
   const [currentUser, setCurrentUser] = useState(null);
-  const auth = useAuth();
-
-  useEffect(() => {
-    if (auth && auth !== false) {
-      setCurrentUser(auth);
-    }
-  }, [auth]);
-
-  const isAuthPage =
-    pathname.startsWith("/sign-in") ||
-    pathname.startsWith("/sign-up");
 
   return (
     <html lang="en" className={roboto.className}>
       <body>
         <UserContext.Provider value={{ currentUser, setCurrentUser }}>
-          
-          {!isAuthPage && currentUser && <Sidebar />}
-
           {children}
         </UserContext.Provider>
       </body>
